62a8451911ed1d0e0347816dc2ba722ea6588a27
lewis.xuwei <lewis.xuwei@outlook.com>
2026-07-07 16:59:55 +0800

    添加LP的约束 	new file:   include/loptw/alg/lp_model.h 	new file:   src/loptw/alg/lp_model.cpp

---
+++ include/loptw/alg/lp_model.h
@@ -0,0 +1,73 @@
+///======================================================
+/// Project :   loptw-alg
+/// File    :   lp_model.h
+///------------------------------------------------------
+/// Time    :   2026/07/05 16:17:15
+/// Author  :   xuwei <lewis.xuwei@outlook.com>
+///
+///           COPYRIGHT @ 2026
+///======================================================
+
+#ifndef __LOPTW_ALG_INCLUDE_LOPTW_ALG_LP_MODEL_H__
+#define __LOPTW_ALG_INCLUDE_LOPTW_ALG_LP_MODEL_H__
+
+#include <memory>
+#include <vector>
+
+#include <loptw/instance/instance.h>
+
+#include <gurobi_c++.h>
+
+namespace loptw::alg {
+
+class LPModel {
+public:
+  LPModel(std::shared_ptr<instance::Instance> inst,
+          std::shared_ptr<std::vector<int>> p,
+          std::shared_ptr<std::vector<int>> r,
+          std::shared_ptr<std::vector<std::vector<int>>> z,
+          std::shared_ptr<std::vector<std::vector<int>>> l,
+          std::shared_ptr<std::vector<std::vector<int>>> m);
+
+  ~LPModel() = default;
+
+  int Optimize();
+
+  double ObjValue();
+  void GetSolution();
+
+private:
+  void AddObjective();
+  void AddConstriants();
+  void AddVariables();
+
+private:
+  std::shared_ptr<instance::Instance> inst;
+  int T;
+  int N;
+  int F;
+
+private:
+  std::shared_ptr<GRBEnv> env;
+  std::shared_ptr<GRBModel> model;
+
+  // ---------- Variables ----------
+  std::vector<GRBVar> x;              // x[N]
+  std::vector<GRBVar> y;              // y[N]
+  std::vector<std::vector<GRBVar>> d; // d[N][N]
+  std::vector<std::vector<GRBVar>> a; // alpha[N][N]
+  std::vector<std::vector<GRBVar>> b; // beta[N][N]
+
+  // ---------- data ----------
+  std::shared_ptr<std::vector<int>> p;
+  std::shared_ptr<std::vector<int>> r;
+  std::shared_ptr<std::vector<std::vector<int>>> z;
+  std::shared_ptr<std::vector<std::vector<int>>> l;
+  std::shared_ptr<std::vector<std::vector<int>>> m;
+
+  int status_; // the status of the reoptimization model
+};
+
+} // namespace loptw::alg
+
+#endif // __LOPTW_ALG_INCLUDE_LOPTW_ALG_LP_MODEL_H__
---
+++ src/loptw/alg/lp_model.cpp
@@ -0,0 +1,193 @@
+///======================================================
+/// Project :   loptw-alg
+/// File    :   lp_model.cpp
+///------------------------------------------------------
+/// Time    :   2026/07/07 15:01:13
+/// Author  :   xuwei <lewis.xuwei@outlook.com>
+///
+///           COPYRIGHT @ 2026
+///======================================================
+
+#include <fmt/format.h>
+
+#include <loptw/alg/lp_model.h>
+
+namespace loptw::alg {
+
+LPModel::LPModel(std::shared_ptr<instance::Instance> inst,
+                 std::shared_ptr<std::vector<int>> p,
+                 std::shared_ptr<std::vector<int>> r,
+                 std::shared_ptr<std::vector<std::vector<int>>> z,
+                 std::shared_ptr<std::vector<std::vector<int>>> l,
+                 std::shared_ptr<std::vector<std::vector<int>>> m) :
+  inst{inst}, p{p}, r{r}, z{z}, l{l}, m{m} {
+  T = inst->GetNumBuildings();
+  N = inst->GetNumTaskNodes();
+  F = inst->GetNumWorkflows();
+
+  env = std::make_shared<GRBEnv>(true);
+  env->set("LogFile", fmt::format("{}.log", inst->instance_path()));
+  env->start();
+
+  model = std::make_shared<GRBModel>(*env);
+}
+
+void LPModel::AddObjective() {
+  GRBLinExpr obj;
+
+  for (int t = 0; t < T; ++t) {
+    double rc = inst->buildings().at(t)->rent_cost_;
+    obj += rc * (*p)[t];
+  }
+
+  for (auto [_, workflow] : inst->workflows()) {
+    int i = workflow->source_;
+    int j = workflow->target_;
+    int f_ij = workflow->quantity_;
+
+    obj += inst->transport_cost() * f_ij * d[i][j];
+  }
+
+  model->setObjective(obj, GRB_MINIMIZE);
+}
+
+void LPModel::AddConstriants() {
+  for (int i = 0; i < N; ++i) {
+    GRBLinExpr expr;
+    for (int t = 0; t < T; ++t) {
+      expr += (*z)[i][t];
+    }
+    model->addConstr(expr == 1, fmt::format("tasknode_placement_{}", i));
+  }
+
+  for (int i = 0; i < N; ++i) {
+    for (int t = 0; t < T; ++t) {
+      GRBLinExpr expr = (*z)[i][t];
+      model->addConstr(expr <= (*p)[t], fmt::format("facility_meet_{}", i));
+    }
+  }
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      double w_i = inst->tasknodes().at(i)->width_;
+      double h_i = inst->tasknodes().at(i)->length_;
+      double w_j = inst->tasknodes().at(j)->width_;
+      double h_j = inst->tasknodes().at(j)->length_;
+
+      model->addConstr(d[i][j] == a[i][j] + b[i][j], fmt::format("distance_{}_{}", i, j));
+      model->addConstr(a[i][j] >= (x[i] + 0.5 * w_i * (1 - (*r)[i]) + 0.5 * h_i * (*r)[i])
+                                    - (x[j] + 0.5 * w_j * (1 - (*r)[j]) + 0.5 * h_j * (*r)[j]),
+                       fmt::format("distance_{}_{}-1", i, j));
+      model->addConstr(a[i][j] >= (x[j] + 0.5 * w_j * (1 - (*r)[j]) + 0.5 * h_j * (*r)[j])
+                                    - (x[i] + 0.5 * w_i * (1 - (*r)[i]) + 0.5 * h_i * (*r)[i]),
+                       fmt::format("distance_{}_{}-2", i, j));
+      model->addConstr(b[i][j] >= (y[i] + 0.5 * h_i * (1 - (*r)[i]) + 0.5 * w_i * (*r)[i])
+                                    - (y[j] + 0.5 * h_j * (1 - (*r)[j]) + 0.5 * w_j * (*r)[j]),
+                       fmt::format("distance_{}_{}-3", i, j));
+      model->addConstr(b[i][j] >= (y[j] + 0.5 * h_j * (1 - (*r)[j]) + 0.5 * w_j * (*r)[j])
+                                    - (y[i] + 0.5 * h_i * (1 - (*r)[i]) + 0.5 * w_i * (*r)[i]),
+                       fmt::format("distance_{}_{}-4", i, j));
+    }
+  }
+
+  for (int t = 0; t < T; ++t) {
+    for (int j = 0; j < N; ++j) {
+      for (int i = 0; i < j; ++i) {
+        GRBLinExpr expr
+          = (*l)[i][j] + (*l)[j][i] + (*m)[i][j] + (*m)[j][i] + (1 - (*z)[i][t]) + (1 - (*z)[j][t]);
+        model->addConstr(expr >= 1, fmt::format("tasknode_relations_{}_{}_{}", i, j, t));
+      }
+    }
+  }
+
+  for (int j = 0; j < N; ++j) {
+    for (int i = 0; i < N; ++i) {
+      double w_i = inst->tasknodes().at(i)->width_;
+      double h_i = inst->tasknodes().at(i)->length_;
+      double w_j = inst->tasknodes().at(j)->width_;
+      double h_j = inst->tasknodes().at(j)->length_;
+
+      model->addConstr(x[i] + w_i * (1 - (*r)[i]) + h_i * (*r)[i]
+                         <= x[j] + 1000000 * (1 - (*l)[i][j]),
+                       fmt::format("task-task-right_{}_left_{}", i, j));
+
+      model->addConstr(y[i] + h_i * (1 - (*r)[i]) + w_i * (*r)[i]
+                         <= y[j] + 1000000 * (1 - (*m)[i][j]),
+                       fmt::format("task-task-top_{}_bottom_{}", i, j));
+    }
+  }
+
+  for (int t = 0; t < T; ++t) {
+    for (int i = 0; i < N; ++i) {
+      double w_i = inst->tasknodes().at(i)->width_;
+      double h_i = inst->tasknodes().at(i)->length_;
+      double W_t = inst->buildings().at(t)->width_;
+      double H_t = inst->buildings().at(t)->length_;
+      double b_t = inst->buildings().at(t)->bottom_coord_;
+      double l_t = inst->buildings().at(t)->left_coord_;
+
+      model->addConstr(x[i] + w_i * (1 - (*r)[i]) + h_i * (*r)[i]
+                         <= l_t + W_t + 1000000 * (1 - (*z)[i][t]),
+                       fmt::format("task-building-right_{}_right_{}", i, t));
+
+      model->addConstr(x[i] >= l_t - 1000000 * (1 - (*z)[i][t]),
+                       fmt::format("task-building-left_{}_left_{}", i, t));
+
+      model->addConstr(y[i] + h_i * (1 - (*r)[i]) + w_i * (*r)[i]
+                         <= b_t + H_t + 1000000 * (1 - (*z)[i][t]),
+                       fmt::format("task-building-bottom_{}_bottom_{}", i, t));
+
+      model->addConstr(y[i] >= b_t - 1000000 * (1 - (*z)[i][t]),
+                       fmt::format("task-building-top_{}_top_{}", i, t));
+    }
+  }
+}
+
+void LPModel::AddVariables() {
+  // x[N]
+  x = std::vector<GRBVar>(N);
+  for (int i = 0; i < N; ++i) {
+    x[i] = model->addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS, fmt::format("x_{}", i));
+  }
+
+  // y[N]
+  y = std::vector<GRBVar>(N);
+  for (int i = 0; i < N; ++i) {
+    y[i] = model->addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS, fmt::format("y_{}", i));
+  }
+
+  // d[N][N]
+  d = std::vector<std::vector<GRBVar>>(N, std::vector<GRBVar>(N));
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      d[i][j] = model->addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS, fmt::format("d_{}_{}", i, j));
+    }
+  }
+  // alpha[N][N]
+  a = std::vector<std::vector<GRBVar>>(N, std::vector<GRBVar>(N));
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      a[i][j] = model->addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS, fmt::format("a_{}_{}", i, j));
+    }
+  }
+
+  // beta[N][N]}
+  b = std::vector<std::vector<GRBVar>>(N, std::vector<GRBVar>(N));
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      b[i][j] = model->addVar(0, GRB_INFINITY, 0, GRB_CONTINUOUS, fmt::format("b_{}_{}", i, j));
+    }
+  }
+}
+
+double LPModel::ObjValue() {
+  return 0;
+}
+
+void LPModel::GetSolution() {}
+
+int LPModel::Optimize() {
+  return 0;
+}
+
+} // namespace loptw::alg
